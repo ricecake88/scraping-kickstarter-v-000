732146fc4d9795893837609f04f20a4dede42daa
Grace Shih <vocalise@gmail.com>
2018-05-20 00:25:18 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,6 +9,8 @@ def create_project_hash
   #projectproj kickstarter.css("li.project.grid_4")
   #title project.css("h2.bbcard_name strong a").text
   #img project.css("div.project-thumbnail a img").attribute("src").value
+  #description: project.css("p.bbcard_blurb").text
+  #location name: project.css("location-name").text
 end
 
 create_project_hash
