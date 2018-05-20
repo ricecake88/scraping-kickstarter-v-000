80f98c366552a87ed720d5ec0a451007da371189
Grace Shih <vocalise@gmail.com>
2018-05-20 00:45:59 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,8 +6,8 @@ def create_project_hash
   html = File.read("fixtures/kickstarter.html")
   kickstarter = Nokogiri::HTML(html)
   binding.pry
-  projects = kickstarter.css("li.project.grid_4")
-  projects.each
+  projects = {}
+  kickstarter.css("li.project.grid_4").each
   #title project.css("h2.bbcard_name strong a").text
   #img project.css("div.project-thumbnail a img").attribute("src").value
   #description: project.css("p.bbcard_blurb").text
