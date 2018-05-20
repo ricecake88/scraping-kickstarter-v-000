3847779369815f1b9328384083b4b1d68295d0c1
Grace Shih <vocalise@gmail.com>
2018-05-20 00:00:50 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,7 +4,7 @@ require 'pry'
 
 def create_project_hash
   html = File.read("fixtures/kickstarter.html")
-  kickstarter = Nokogirl::HTML(html)
+  kickstarter = Nokogiri::HTML(html)
   binding.pry
 end
 
